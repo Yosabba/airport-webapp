a2255d0ced02ce0cb1268ed617907215c6754f21
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-18 20:19:52 -0400

    connected to yelp api to get data based on location

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const yelp = require("yelp-fusion");
+const dotenv = require("dotenv");
+const client = yelp.client(process.env.APP_YELP_KEY);
+
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hello World!" });
+app.get("/", async (req, res) => {
+  try {
+    const response = await client.search({
+      term: "Four Barrel Coffee",
+      location: "san francisco, ca",
+    });
+
+    res.json({ data: response.jsonBody.businesses });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 });
 app.listen(PORT, () => {
   console.log("Connected to database");
