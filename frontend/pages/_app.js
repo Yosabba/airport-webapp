779454bbc72e300150d4e6273812f3ffaca422db
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-18 23:25:54 -0400

    added custom breakpoints for all devices and added redux provider

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,40 @@
 import { Provider } from "react-redux";
+import { store } from "../store.js";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const breakpoints = {
+  sm: "320px",
+  md: "768px",
+  lg: "960px",
+  xl: "1200px",
+  "2xl": "1536px",
+};
+
+const customTheme = {
+  fonts: {
+    body: "Poppins, sans-serif",
+    heading: "Poppins, sans-serif",
+  },
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "#F5f5f5",
+      },
+    },
+  },
+};
+
+const theme = extendTheme({ breakpoints });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
