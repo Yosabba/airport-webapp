f008d3a8b20467204c213df1ba013b6efdb64405
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-18 23:24:07 -0400

    removed old unused files

--- frontend/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import airportState from "./airportState";
-
-const rootReducer = combineReducers({
-    airport: airportState
-});
-
-export default rootReducer;
