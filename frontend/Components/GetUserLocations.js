6303f48bc5b216b7d6006be3464bf99dc4092872
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-20 00:21:06 -0400

    Stlying for landing page complete

--- frontend/Components/GetUserLocations.js
+++ frontend/Components/GetUserLocations.js
@@ -6,21 +6,41 @@ import {
   Input,
   Flex,
   Button,
+  Heading,
 } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import React, { useEffect } from "react";
 
 const GetUserLocation = () => {
   return (
-    <Flex justify="center" wrap="wrap">
-      <Text>Enter your airport</Text>
+    <Flex justify="center" wrap="wrap" direction="column" zIndex="999">
+      <Heading as="h1" my="4rem" color="white" zIndex="90">
+        Welcome to Airportly
+      </Heading>
+      <Text align="left" color="white" zIndex="20">
+        Enter your airport
+      </Text>
       <Input
         maxLength="3"
         size="sm"
         type="text"
         placeholder="Ex: LAX"
         textTransform="uppercase"
+        bg="white"
+        zIndex="20"
+        borderColo="white"
+        outline="none"
+        _focus={{
+          transition: "all 0.2s ease-in-out",
+        }}
       />
-      <Button bg="green.400" mt="1rem">
+      <Button
+        colorScheme="messenger"
+        w="10vw"
+        mx="auto"
+        my="3"
+        _hover={{ transition: "all .2s ease-in-out", transform: "scale(1.1)" }}
+      >
         Search
       </Button>
     </Flex>
