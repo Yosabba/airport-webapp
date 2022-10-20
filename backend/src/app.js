ed504be45685c9ae7dffa879d9fa177af79ccc7d
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-20 19:49:40 -0400

    added function to handle all unexpected errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+const errorHandler = require('./errors/errorHandler')
 
 const foodRouter = require("./food/food.router.js");
 
@@ -11,8 +12,12 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use("/food", foodRouter);
+app.use(errorHandler)
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Food API");
+});
 
 app.listen(PORT, () => {
-  console.log("Connected to database");
   console.log(`Server is running on port ${PORT}`);
 });
