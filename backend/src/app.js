b7b726c771f93ae8eeed53b3c5ba84ad236a79f0
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-19 23:04:48 -0400

    refactored code moved logic into controller file and routes to router file then import them

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,30 +2,16 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
-const yelp = require("yelp-fusion");
-const dotenv = require("dotenv");
-dotenv.config();
-const client = yelp.client(process.env.APP_YELP_KEY);
+
+const foodRouter = require("./food/food.router.js");
 
 const PORT = process.env.PORT || 5000;
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+app.use("/food", foodRouter);
 
-app.get("/", async (req, res) => {
-  try {
-    const response = await client.search({
-      term: "Four Barrel Coffee",
-      location: "san francisco, ca",
-    });
-
-    res.json({ data: response.jsonBody.businesses });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Something went wrong" });
-  }
-});
 app.listen(PORT, () => {
   console.log("Connected to database");
   console.log(`Server is running on port ${PORT}`);
