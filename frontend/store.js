a8b4be3365e1c27cc459138a476e36e660d2cc09
Dominic Yosabba Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-27 12:58:17 -0400

    setup for RTK query

--- frontend/store.js
+++ frontend/store.js
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import foodLocationReducer from "./features/food-location/food-location-slice";
-// import { apiSlice } from "./features/food-location/food-location-slice";
+import { apiSlice } from "./features/food-location/api-slice";
 
 export const store = configureStore({
   reducer: {
     food: foodLocationReducer,
-    // [apiSlice.reducerPath]: apiSlice.reducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  // middleware: (getDefaultMiddleware) => {
-  //   return getDefaultMiddleware().concat(apiSlice.middleware);
-  // },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(apiSlice.middleware);
+  },
 });
