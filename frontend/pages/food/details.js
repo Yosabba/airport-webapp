dafd936389cb0cae067340b5a8c0bf184f3027c0
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-31 13:04:44 -0400

    fixing bug

--- frontend/pages/food/details.js
+++ frontend/pages/food/details.js
@@ -11,7 +11,7 @@ export default function Details() {
     const { businessDetails } = useSelector((state) => state.food);
 
     setBusiness(businessDetails);
-  }, [businessDetails]);
+  }, []);
 
   return (
     <main>
