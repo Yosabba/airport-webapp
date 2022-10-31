e14ccf06580054c7f9c493b90eac971b8173fb61
Dominic Johnson <85464248+Yosabba@users.noreply.github.com>
2022-10-31 12:44:49 -0400

    details page to show business details

---
+++ frontend/pages/food/details.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Box, Heading, SimpleGrid, Flex, Text } from "@chakra-ui/react";
+import BusinessDetails from "../../Components/Food-Details";
+import { useSelector } from "react-redux";
+export default function Details() {
+  const { businessDetails } = useSelector((state) => state.food);
+  return (
+    <main>
+      <Head>
+        <title>Airportly</title>
+        <meta
+          name="description"
+          content="Find food in your terminal and get directions to it"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, minimum-scale=1.0, maximum-scale = 1.0, user-scalable = no"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {businessDetails && <BusinessDetails businessDetails={businessDetails} />}
+    </main>
+  );
+}
